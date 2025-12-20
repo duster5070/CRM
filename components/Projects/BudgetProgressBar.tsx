303958e1ba90f3705d72c81fa834e6208e193ee7
Ahmed Mohammed <ahmedelmdfagia@gmail.com>
2025-12-20 21:51:45 +0200

    Creating the payment progressbar component

---
+++ components/Projects/BudgetProgressBar.tsx
@@ -0,0 +1,42 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+interface BudgetProgressBarProps {
+  budget: number;
+  paidAmount: number;
+}
+
+const BudgetProgressBar: React.FC<BudgetProgressBarProps> = ({
+  budget,
+  paidAmount,
+}) => {
+  const remainingAmount = Math.max(budget - paidAmount, 0);
+  const paidPercentage = Math.min((paidAmount / budget)* 100, 100);
+
+  return (
+    <div className='w-full border-t pt-3'>
+      <div className='flex justify-between mb-2'>
+      <span className='text-sm font-medium text-gray-700'>
+        Payment Progress (<span className='text-xs'>{`${paidPercentage.toFixed(0)}%`}</span>)
+      </span>
+      <span className='text-sm font-medium text-gray-700'> {`$${paidAmount.toLocaleString()} / $${budget.toLocaleString()}`} </span>
+      </div>
+      <div className='w-full bg-gray-200 rounded-full h-2.5'>
+        <div
+          className={cn('bg-yellow-400 h-2.5 rounded-full', paidPercentage > 50 && 'bg-green-600')}
+          style={{ width: `${paidPercentage}%`}}
+          ></div>
+      </div>
+      <div className='flex justify-between mt-2'>
+        <span className='text-sm text-gray-600'>
+          Paid: ${paidAmount.toLocaleString()}
+        </span>
+        <span className='text-sm text-gray-600'>
+          Remaining: ${remainingAmount.toLocaleString()}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default BudgetProgressBar 
