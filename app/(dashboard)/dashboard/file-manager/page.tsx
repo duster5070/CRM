27426b0c11f4dab5f9cbc165b1e195e1c53404a6
kisk <tarekanwer23@yahoo.com>
2025-12-25 05:08:57 +0200

    adding the file management

---
+++ app/(dashboard)/dashboard/file-manager/page.tsx
@@ -0,0 +1,27 @@
+import { getDetailedUserProjects } from "@/actions/projects";
+import NotFound from "@/app/not-found";
+import { FileManager } from "@/components/dashboard/FileManager";
+import PaymentsPage from "@/components/dashboard/PaymentsPage";
+import { getAuthUser } from "@/config/useAuth";
+
+export default async function Page() {
+  const user = await getAuthUser();
+
+  if (!user) {
+    return (
+      <div className="flex h-screen items-center justify-center text-gray-500">
+        Please sign in to view your payments.
+      </div>
+    );
+  }
+
+  // const userProjects = await getDetailedUserProjects(user.id);
+
+  // if (!userProjects || userProjects.length === 0) {
+  //   return (
+  //    <NotFound/>
+  //   );
+  // }
+
+  return <FileManager />;
+}
